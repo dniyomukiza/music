0e067163367381ed1be538cd0aefcc2d85dff59c
Nididier <76929907+dniyomukiza@users.noreply.github.com>
2025-04-18 19:36:15 -0400

    mounting disk

--- start_all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Start all services in the background
-python /appdir/run.py &
-uvicorn /appdir/glconnect.voc:app --reload --host 0.0.0.0 --port 8001 &
-/usr/bin/icecast -c /etc/icecast.xml -b &
-liquidsoap /liqfolder/scripts/main.liq
