f7342f93ddeb53abdf9a4f061e315bd89494be1c
Nididier <76929907+dniyomukiza@users.noreply.github.com>
2025-06-05 04:57:37 -0400

    radio stopped

--- glconnect/__init__.py
+++ glconnect/__init__.py
@@ -19,7 +19,7 @@ with open('/etc/glconfig.json') as json_file:
 
 def create_app():
     app = Flask(__name__)
-    CORS(app, supports_credentials=True, origins=['https://www.glc.cool'])
+    '''CORS(app, supports_credentials=True, origins=['https://www.glc.cool'])
     @app.after_request
     def after_request(response):
         origin = request.headers.get('Origin')
@@ -37,6 +37,7 @@ def create_app():
         SESSION_COOKIE_HTTPONLY=True,
         SESSION_COOKIE_DOMAIN='.glc.cool'
     )
+     '''
     app.secret_key = os.urandom(24)
     ckeditor = CKEditor() 
     app.config['CKEDITOR_SERVE_LOCAL'] = True
