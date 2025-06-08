23fef16d5fd2e7adf8cef35f8838a12fe84a1e0f
Nididier <76929907+dniyomukiza@users.noreply.github.com>
2025-06-08 14:16:20 -0400

    added session in routes1

--- glconnect/__init__.py
+++ glconnect/__init__.py
@@ -22,7 +22,7 @@ def create_app():
     app = Flask(__name__)
 
     # CORS configuration - allow both glc.cool and www.glc.cool
-    CORS(art, supports_credentials=True, origins=[
+    CORS(app, supports_credentials=True, origins=[
         "https://www.glc.cool"
     ])
 
